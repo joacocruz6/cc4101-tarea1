56cf1f6d8c4078e7d1b47d12b8afa2883cf4bf21
Joaquin Cruz <joacocruz6@gmail.com>
2019-04-28 16:48:03 -0400

    Finished the details of documentation

--- src/main.rkt
+++ src/main.rkt
@@ -12,10 +12,7 @@
          | {- <s-expr> <s-expr>}
          | {with {<s-expr> : <type> <s-expr>} <s-expr>}
          | {fun {<id>  : <s-expr>} [: <type>] <expr>}
-         | {<expr> <expr>}         
- 
-<type> ::= Num
-         | {<type> -> <type>}}
+         | {<expr> <expr>}
 |#
 (deftype Expr
   (num n)
@@ -26,7 +23,10 @@
   (fun-db body) ;tambien se usa en la p3
   (acc n) ;Se usa para la pregunta 3
   (app fun-id arg-expr))
-
+#|
+<type> ::= Num
+         | {<type> -> <type>}}
+|#
 (deftype Type
   (TNum)
   (TFun arg ret))
@@ -53,8 +53,8 @@ Busca el tipo de la variable x en el ambiente, si no esta tira error de identifi
 
 #|
 parse-type: List[Symbol] -> Type (o error)
-Parsea el tipo de el contrato de una función.
-Por ejemplo: parse-type {Num -> Num} debe dar (TNum) (TNum)
+Parsea el tipo de el contrato de una función. Puede generar error de tipos
+si uno de los dos no es encontrado.
 |#
 (define (parse-type s-expr)
   (match s-expr
@@ -82,35 +82,40 @@ Parsea una lista de simbolos a un AST del lenguaje con tipos.
     [(list 'with {list args ': args-type expr-value} body) ;parseo del with
      (app (parse {list 'fun {list args ': args-type} body}) (parse expr-value))]
     [(list expr1 expr2) (app (parse expr1) (parse expr2))]))
-
+#|
+prettify :: Type -> List[Symbol] (o un symbolo si es TNum)
+Dado un tipo del lenguaje se transforma en sintaxis concreta.
+|#
 (define (prettify t-expr)
   (match t-expr
   [(TNum) 'Num]
   [(TFun t-arg t-ret) (list (prettify t-arg) '-> (prettify t-ret))]))
+
 #|
-typeof :: Expr Env -> Type (o error)
+typeof-ext :: Expr TypeEnv -> Type (o error)
 Dada una expresion parseada y un ambiente, determina el tipo de esa expresión.
+Puede generar error de tipos en caso de que haya en el programa escrito
 |#
-(define (typeof expr [env empty-type-env])
+(define (typeof-ext expr [env empty-type-env])
   (match expr
     [(num n) (TNum)]
     [(id x) (lookup-type-env x env)]
-    [(add l r) (def type-right (typeof r env))
-                (def type-left (typeof l env))
+    [(add l r) (def type-right (typeof-ext r env))
+                (def type-left (typeof-ext l env))
                 (if (and (equal? type-right (TNum)) (equal? type-left (TNum)))
                    (TNum)
                    (if (not (equal? type-right (TNum)))
                        (error "Type error in expression + position 2: expected Num found" (prettify type-right))
                        (error "Type error in expression + position 1: expected Num found" (prettify type-left))))]
-    [(sub l r) (def type-right (typeof r env))
-                (def type-left (typeof l env))
+    [(sub l r) (def type-right (typeof-ext r env))
+                (def type-left (typeof-ext l env))
                 (if (and (equal? type-right (TNum)) (equal? type-left (TNum)))
                    (TNum)
                    (if (not (equal? type-right (TNum)))
                        (error "Type error in expression - position 2: expected Num found" (prettify type-right))
                        (error "Type error in expression - position 1: expected Num found" (prettify type-left))))]
     [(fun args args-type body ret-type)
-     (def body-type (typeof body (extend-type-env args args-type env)))
+     (def body-type (typeof-ext body (extend-type-env args args-type env)))
      (if (or (equal? ret-type body-type) (equal? ret-type #f))
          (TFun args-type body-type)
          (error "Type error in expression fun position 1: expected" (prettify ret-type) 'found (prettify body-type)))] ;TODO,el error que se debe lanzar y ver el caso cuando ret-type vale falso.
@@ -119,24 +124,33 @@ Dada una expresion parseada y un ambiente, determina el tipo de esa expresión.
        [(id x) ;si es un id, lo busco en el ambiente y debe ser tipo funcion
         (match (lookup-type-env x env)
           [(TNum) (error "Type error in expression app position 1: expected (T -> S) found Num")]
-          [(TFun args-type ret-type) (if (equal? args-type (typeof expr2))
+          [(TFun args-type ret-type) (if (equal? args-type (typeof-ext expr2 env))
                                                  ret-type
-                                                 (error "Type error in expression app position 2: expected" (prettify args-type) 'found (prettify (typeof expr2))))])]; si x es TNum, esta mal, si es un TFun veo que el tipo de de args sea el tipo de expr2 y retorno el tipo de su body.
+                                                 (error "Type error in expression app position 2: expected" (prettify args-type) 'found (prettify (typeof-ext expr2 env))))])]; si x es TNum, esta mal, si es un TFun veo que el tipo de de args sea el tipo de expr2 y retorno el tipo de su body.
        [(fun args args-type body #f) ;si es tipo funcion sin retorno, veo si los argumentos calzan y devuelvo el typo funcion con el tipo de su body
-        (if (equal? args-type (typeof expr2 env))
-            (typeof body (extend-type-env args args-type env))
-            (error "Type error in expression app position 2: expected" (prettify args-type) 'found (prettify (typeof expr2))))] ; veo si el tipo de argumentos es el mismo que el de los parametros y extiendo el ambiente
+        (if (equal? args-type (typeof-ext expr2 env))
+            (typeof-ext body (extend-type-env args args-type env))
+            (error "Type error in expression app position 2: expected" (prettify args-type) 'found (prettify (typeof-ext expr2 env))))] ; veo si el tipo de argumentos es el mismo que el de los parametros y extiendo el ambiente
        [(fun args args-type body body-type) ;si tiene tipo funcion anotado, veo si los argumentos y el body calzan
-        (if (equal? args-type (typeof expr2 env))
-            (if (equal? (typeof body (extend-type-env args args-type env)))
-                (TFun args-type body-type)
-                (error "Type error in expression app position 1: expected" (prettify body-type) 'found (prettify (typeof body))))
-            (error "Type error in expression app position 2: expected" (prettify args-type) 'found (prettify (typeof expr2)))
+        (if (equal? args-type (typeof-ext expr2 env))
+            (if (equal? body-type (typeof-ext body (extend-type-env args args-type env)))
+                body-type
+                (error "Type error in expression app position 1: expected" (prettify body-type) 'found (prettify (typeof-ext body env))))
+            (error "Type error in expression app position 2: expected" (prettify args-type) 'found (prettify (typeof-ext expr2 env)))
             )]       
-       [ _ (error "Type error in expression app position 1: expected (T -> S) found" (prettify (typeof expr1 env)))] ;cualquier otra cosa esta mala
+       [ _ (error "Type error in expression app position 1: expected (T -> S) found" (prettify (typeof-ext expr1 env)))] ;cualquier otra cosa esta mala
     )]))
 #|
-TODO
+typeof :: Expr -> Type (o error)
+Dada una expresion parseada, determina el tipo de esa expresión.
+Puede generar error de tipos en caso de encontrarlo.
+|#
+(define (typeof expr)
+  (typeof-ext expr))
+#|
+typecheck :: List[Symbol] -> List[Symbol] (o error)
+Dada una lista de simbolos que representan un programa en el lenguaje definido, se entrega una lista de symbolos.
+Puede generar error de tipos en caso que se de en el programa escrito.
 |#
 (define (typecheck s-expr)
   (prettify (typeof (parse s-expr))))
@@ -145,14 +159,19 @@ TODO
 ;;;;;;;;;
 ;;;P3;;;;
 ;;;;;;;;;
+
+#|
+Declaración sobre el ambiente de bruijn
+|#
 (deftype Env
   (BruijnEnv id next))
 (define empty-bruijn-env (mtEnv))
 (define extend-bruijn-env BruijnEnv)
 #|
-lookup-bruijn-env::= <id> Env -> <acc> (error)
+lookup-bruijn-env ::= <id> Env -> <acc> (o error)
 Busca en el ambiente de bruijn index el id x, y da la expression acc con
-su indice asociado.
+su indice asociado. Genera error al no encontrar el identificadore en el ambiente
+asociandolo a un identificador libre.
 |#
 (define (lookup-bruijn-env x env [pos 0])
   (match env
@@ -162,18 +181,31 @@ su indice asociado.
          (acc pos)
          (lookup-bruijn-env x next (+ pos 1)))]))
 #|
-TODO
+deBruijn-ext :: Expr Env -> Expr (o error) 
+Dada un expresion parseada y un ambiente genera la representacion con indices deBruijn de la expresion.
+Puede generar error de identificador libre en caso de que encuentre uno.
 |#
-(define (deBruijn expr [env empty-bruijn-env])
+(define (deBruijn-ext expr [env empty-bruijn-env])
   (match expr
     [(num n) (num n)]
     [(id x) (lookup-bruijn-env x env)]
-    [(add l r) (add (deBruijn l env) (deBruijn r env))]
-    [(sub l r) (sub (deBruijn l env) (deBruijn r env))]
-    [(fun args args-type body body-type) (fun-db (deBruijn body (extend-bruijn-env args env)))]
-    [(app (fun args args-type body body-type) expr2) (app (fun-db (deBruijn body (extend-bruijn-env args env))) (deBruijn expr2 env))]
-    [(app (id x) expr2) (app (lookup-bruijn-env x env) (deBruijn expr2 env))]))
-
+    [(add l r) (add (deBruijn-ext l env) (deBruijn-ext r env))]
+    [(sub l r) (sub (deBruijn-ext l env) (deBruijn-ext r env))]
+    [(fun args args-type body body-type) (fun-db (deBruijn-ext body (extend-bruijn-env args env)))]
+    [(app (fun args args-type body body-type) expr2) (app (fun-db (deBruijn-ext body (extend-bruijn-env args env))) (deBruijn-ext expr2 env))]
+    [(app (id x) expr2) (app (lookup-bruijn-env x env) (deBruijn-ext expr2 env))]))
+#|
+deBruijn :: Expr -> Expr (error)
+Dada una expresion parseada genera la representación con indices deBruijn de esa expresion. Genera error de identificador libre
+cuando sea necesario.
+|#
+(define (deBruijn expr)
+  (deBruijn-ext expr))
+#|
+compile :: Expr -> List[Instructions]
+Toma una lista del lenguajes con indices deBruijn ya puestos y la convierte
+a una lista del set de instrucciones de maquina a utilizar en esta tarea.
+|#
 (define (compile expr)
   (match expr
     [(num n) (list (INT-CONST n))]
@@ -182,7 +214,11 @@ TODO
     [(sub l r) (append (compile r) (compile l) (list (SUB)))]
     [(fun-db body) (list (CLOSURE (append (compile body) (list (RETURN)))))]
     [(app expr1 expr2) (append (compile expr2) (compile expr1) (list (APPLY)))]))
-
+#|
+typed-compile :: List[Symbol] -> List[Instructions] (o error)
+Dada una lista de symbolos en el lenguaje, se genera una lista de instrucciones de la maquina dada por enunciado.
+Esto además parsea al lenguaje y comprueba los tipos de las expresiones en pasos intermedios.
+|#
 (define (typed-compile s-expr)
   (def parsed-expr (parse s-expr))
   (def type-check-expr (typecheck s-expr))
--- src/test.rkt
+++ src/test.rkt
@@ -2,6 +2,12 @@
 (require "main.rkt")
 (require "machine.rkt")
 
+;;lookup-type-env
+(test (lookup-type-env 'x (extend-type-env 'x (TNum) empty-type-env)) (TNum))
+(test (lookup-type-env 'x  (extend-type-env 'x (TFun (TNum) (TNum)) empty-type-env)) (TFun (TNum) (TNum)))
+(test/exn (lookup-type-env 'x empty-type-env) "Type error: free identifier: x")
+(test/exn (lookup-type-env 'y (extend-type-env 'x (TNum) (extend-type-env 'z (TFun (TNum) (TNum)) empty-type-env))) "Type error: free identifier: y")
+
 ;; parse-type
 (test (parse-type '{Num}) (TNum))
 (test (parse-type '{Num -> Num}) (TFun (TNum) (TNum)))
@@ -9,7 +15,7 @@
 (test (parse-type '{{Num -> Num} -> {Num -> Num}}) (TFun (TFun (TNum) (TNum)) (TFun (TNum) (TNum))))
 (test/exn (parse-type '{Num -> }) "Parse error")
 (test/exn (parse-type '{ -> Num}) "Parse error")
-(test/exn (parse-type '{{Num -> Num} -> {Num -> }}) "Parse error" )
+(test/exn (parse-type '{{Num -> Num} -> {Num -> }}) "Parse error")
 
 ;;prettify tests
 (test (prettify (TNum)) 'Num)
@@ -23,23 +29,34 @@
 (test (parse '{with {y : Num 2} {+ x y}}) (app (fun 'y (TNum) (add (id 'x) (id 'y)) #f) (num 2)))
 (test (parse '{with {y : Num 2} y}) (app (fun 'y (TNum) (id 'y) #f) (num 2)))
 (test (parse '{y 2}) (app (id 'y) (num 2)))
+(test (parse '{{fun {x : Num} : Num {+ x 10}} 2}) (app (fun 'x (TNum) (add (id 'x) (num 10)) (TNum)) (num 2)))
 (test (parse '{fun {x : Num} : Num {+ x 1}}) (fun 'x (TNum) (add (id 'x) (num 1)) (TNum)))
 (test (parse '{with {y : Num 2} {+ x y}}) (app (fun 'y (TNum) (add (id 'x) (id 'y)) #f) (num 2)))
 (test (parse '{with {x : Num 5} {+ x 3}}) (app (fun 'x (TNum) (add (id 'x) (num 3)) #f) (num 5)))
 (test (parse '{{fun {x : Num} : Num {+ x 1}} 2}) (app (fun 'x (TNum) (add (id 'x) (num 1)) (TNum)) (num 2)))
 (test (parse '{with {y : {Num -> Num} {fun {x : Num} : Num {+ x x}}} {y 2}}) (app (fun 'y (TFun (TNum) (TNum)) (app (id 'y) (num 2)) #f) (fun 'x (TNum) (add (id 'x) (id 'x)) (TNum))))
-;;lookup-env
-(test (lookup-type-env 'x (extend-type-env 'x (TNum) empty-type-env)) (TNum))
-(test (lookup-type-env 'x  (extend-type-env 'x (TFun (TNum) (TNum)) empty-type-env)) (TFun (TNum) (TNum)))
-(test/exn (lookup-type-env 'x empty-type-env) "Type error: free identifier: x")
 
+;;typeof-ext
+(test (typeof-ext (parse '3)) (TNum)) ;Test para verificar tipo de los numeros
+(test (typeof-ext (parse '{+ 1 3})) (TNum)) ;Test para verificar tipo de la adicion numerica
+(test (typeof-ext (parse '{fun {x : Num} : Num 5})) (TFun (TNum) (TNum))) ;Test para verificar 
+(test (typeof-ext (parse '{{fun {x : Num} x} 1})) (TNum)) ;test para verificar correctitud de aplicaciones
+(test (typeof-ext (parse '{{fun {f : {Num -> Num}} {f 12}} {fun {x : Num} {+ x x}}})) (TNum))
+(test/exn (typeof-ext (parse 'y)) "Type error: free identifier: y") ;test error para variables libres
+(test/exn (typeof-ext (parse '{fun {x : Num} : {Num -> Num} 10})) "Type error in expression fun position 1: expected (Num -> Num) found Num")
+(test/exn (typeof-ext (parse '{1 2})) "Type error in expression app position 1: expected (T -> S) found Num")
+(test/exn (typeof-ext (parse '{{fun {x : Num} : Num {+ x x}} {fun {x : Num} : Num 5}})) "Type error in expression app position 2: expected Num found (Num -> Num)")
+(test/exn (typeof-ext (parse '{y 2})) "Type error: free identifier: y")
+(test/exn (typeof-ext (parse '{- {fun {x : Num} : Num {+ x x}} 2})) "Type error in expression - position 1: expected Num found (Num -> Num)")
 
-;;typeof
-(test (typeof (parse '3)) (TNum)) ;Test para verificar tipo de los numeros
+;;typeof (son los mismos test que antes, verificando que hace lo correcto)
+(test (typeof (parse '3)) (TNum)) 
 (test (typeof (parse '{+ 1 3})) (TNum)) ;Test para verificar tipo de la adicion numerica
 (test (typeof (parse '{fun {x : Num} : Num 5})) (TFun (TNum) (TNum))) ;Test para verificar 
 (test (typeof (parse '{{fun {x : Num} x} 1})) (TNum)) ;test para verificar correctitud de aplicaciones
 (test (typeof (parse '{{fun {f : {Num -> Num}} {f 12}} {fun {x : Num} {+ x x}}})) (TNum))
+(test (typeof (parse '{{fun {x : Num} : Num {+ x 10}} 2})) (TNum))
+(test (typeof (parse '{+ 1 {{fun {x : Num} : Num {+ x 10}} 2}})) (TNum))
 (test/exn (typeof (parse 'y)) "Type error: free identifier: y") ;test error para variables libres
 (test/exn (typeof (parse '{fun {x : Num} : {Num -> Num} 10})) "Type error in expression fun position 1: expected (Num -> Num) found Num")
 (test/exn (typeof (parse '{1 2})) "Type error in expression app position 1: expected (T -> S) found Num")
@@ -47,7 +64,7 @@
 (test/exn (typeof (parse '{y 2})) "Type error: free identifier: y")
 (test/exn (typeof (parse '{- {fun {x : Num} : Num {+ x x}} 2})) "Type error in expression - position 1: expected Num found (Num -> Num)")
 
-;typecheck
+;;typecheck
 (test (typecheck '3) 'Num)
 (test (typecheck '{fun {f : Num} : Num 10}) '(Num -> Num))
 (test/exn (typecheck  '{+ 2 {fun {x : Num} : Num x}}) "Type error in expression + position 2: expected Num found (Num -> Num)") ;TODO este test no pasa
@@ -59,7 +76,21 @@
 (test (lookup-bruijn-env 'x (extend-bruijn-env 'y (extend-bruijn-env 'x empty-bruijn-env))) (acc 1))
 (test/exn (lookup-bruijn-env 'x (extend-bruijn-env 'y (extend-bruijn-env 'z empty-bruijn-env))) "Free identifier: x")
 (test/exn (lookup-bruijn-env 'x empty-bruijn-env) "Free identifier: x")
-;; deBruijn
+
+;; deBruijn-ext
+(test (deBruijn-ext (num 3)) (num 3))
+(test (deBruijn-ext (parse '{+ 3 3})) (add (num 3) (num 3)))
+(test (deBruijn-ext (parse '{- 3 3})) (sub (num 3) (num 3)))
+(test (deBruijn-ext (parse '{with {x : Num 5}  {with  {y : Num  {+ x 1}} {+ y x}}}))
+      (app (fun-db (app (fun-db (add (acc 0) (acc 1))) (add (acc 0) (num 1)))) (num 5)))
+(test (deBruijn-ext (parse '{with {x : {Num -> Num} {fun {y : Num} : Num (+ y y)}}
+                              {with {y : Num 2}
+                                    {x y}}})) (app (fun-db (app (fun-db (app (acc 1) (acc 0))) (num 2))) (fun-db (add (acc 0) (acc 0)))))
+(test (deBruijn-ext (fun 'x (TNum) (add (id 'x) (num 1)) (TNum))) (fun-db (add (acc 0) (num 1))))
+(test (deBruijn-ext (parse '(fun {x : Num} : {Num -> Num} 10 )))  (fun-db (num 10)))
+(test/exn (deBruijn-ext (parse 'x)) "Free identifier: x")
+
+;; deBruijn (son los mismos test pues esta funcion debe actuar igual que la anterior)
 (test (deBruijn (num 3)) (num 3))
 (test (deBruijn (parse '{+ 3 3})) (add (num 3) (num 3)))
 (test (deBruijn (parse '{- 3 3})) (sub (num 3) (num 3)))
@@ -77,7 +108,17 @@
 (test (compile (num 1)) (list (INT-CONST 1)))
 (test (compile (sub (num 1) (num 1))) (list (INT-CONST 1) (INT-CONST 1) (SUB)))
 (test (compile (add (num 2) (num 1))) (list (INT-CONST 1) (INT-CONST 2) (ADD)))
+(test (compile (deBruijn (parse '{with {x : Num 2} {+ x x}}))) (list (INT-CONST 2) (CLOSURE (list (ACCESS 0) (ACCESS 0) (ADD) (RETURN))) (APPLY)))
 (test (compile (deBruijn (parse '{{fun {x : Num} : Num {+ x 10}} {+ 2 3}}))) (list (INT-CONST 3) (INT-CONST 2) (ADD) (CLOSURE (list (INT-CONST 10) (ACCESS 0) (ADD) (RETURN))) (APPLY))) 
 
 ;;typed-compile
 (test (typed-compile '{+ 1 2}) (list (INT-CONST 2) (INT-CONST 1) (ADD)))
+(test (typed-compile '{+ 1 {{fun {x : Num} : Num {+ x 10}} 2}}) (list (INT-CONST 2) (CLOSURE (list (INT-CONST 10) (ACCESS 0) (ADD) (RETURN))) (APPLY) (INT-CONST 1) (ADD)))
+(test (typed-compile '{with {y : {Num -> Num} {fun {x : Num} : Num {+ x x}}}
+                        {with {x : {Num -> Num} {fun {y : Num} : Num {+ y 4}}}
+                              {- {y 8} {x 3}}}}) (list
+ (CLOSURE (list (ACCESS 0) (ACCESS 0) (ADD) (RETURN)))
+ (CLOSURE (list (CLOSURE (list (INT-CONST 4) (ACCESS 0) (ADD) (RETURN))) (CLOSURE (list (INT-CONST 3) (ACCESS 0) (APPLY) (INT-CONST 8) (ACCESS 1) (APPLY) (SUB) (RETURN))) (APPLY) (RETURN)))
+ (APPLY)))
+(test/exn (typed-compile '{+ 1 {fun {x : Num} : Num {+ x 10}}}) "Type error in expression + position 2: expected Num found (Num -> Num)")
+(test/exn (typed-compile '{+ 1 x}) "Type error: free identifier: x")
